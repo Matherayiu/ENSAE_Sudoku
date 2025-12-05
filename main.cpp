64035e2e5afc6b6290a20556df2a096f7a18154f
Matherayiu <theophile.roulland-durande@ensae.fr>
2025-12-05 10:51:32 +0000

    Edit grid display : add numbers

--- main.cpp
+++ main.cpp
@@ -103,9 +103,18 @@ int solution4[9][9] = {
 };
 
 void afficherGrille(int g[9][9]) {
-    cout << " -----------------------" << endl;
+    cout << "  ";
     for (int i = 0; i < 9; i++) {
-        cout << "| ";
+        if (i==2 || i==5){
+            cout << " " << i+1<<"  ";
+        }
+        else {
+            cout << " " << i+1;
+        }
+    }
+    cout << endl; cout << " -----------------------" << endl;
+    for (int i = 0; i < 9; i++) {
+        cout << i+1 << "| ";
         for (int j = 0; j < 9; j++) {
             if (g[i][j] == 0)
                 cout << ". ";
